7c299dfaa8c10bf7ad47a2c39e43a433e2a2b83a
Daniel Karani <danielevans.karani@gmail.com>
2023-08-08 16:31:49 +0300

    Adds two fields and user types to the user model

--- attachment/models.py
+++ attachment/models.py
@@ -30,34 +30,48 @@ class UserManager(BaseUserManager):
             password=password,
 
         )
-         
+
         user.is_admin = True
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
         return user
 
+
 class User(AbstractBaseUser):
     
-    email = models.EmailField(max_length=100, unique = True)
+    
+    USER_TYPE_CHOICES = (
+
+        ('admin', "Admin"),
+        ('employer', "Employer"),
+        ('supervisor', "Supervisor"),
+        ('student', "Student"),
+    )
+
+    email = models.EmailField(max_length=100, unique=True)
     username = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=10)
     registration_number = models.CharField(max_length=255, unique=True)
     staff_number = models.CharField(max_length=255, unique=True)
     employee_id = models.CharField(max_length=255, unique=True)
+    organisation = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    
+    REQUIRED_FIELDS = ['username', 'user_type']
+
     objects = UserManager()
-    
+
     def __str__(self):
         return self.email
-    
+
+
 class Student(models.Model):
-    
-    name =  models.CharField(max_length=255)
+
+    name = models.CharField(max_length=255)
     date_of_birth = models.DateField()
     course = models.CharField(max_length=255)
     year = models.IntegerField(blank=True, null=True)
@@ -68,12 +82,11 @@ class Student(models.Model):
     end_date = models.DateField(default=datetime.date.today)
     assessed_by_employer = models.BooleanField(default=False)
     assessed_by_lecturer = models.BooleanField(default=False)
-    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    
+    user = models.OneToOneField(
+        User, primary_key=True, on_delete=models.CASCADE)
+
+
 class LogBook(models.Model):
     date = models.DateField(auto_now=False)
     description = models.TextField(max_length=500)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    
-
-
