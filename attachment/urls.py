f15d0655b00c1624b8c058826adbbeccc4ff3ccb
Daniel Karani <danielevans.karani@gmail.com>
2023-08-11 10:07:12 +0300

    Adds the login url

--- attachment/urls.py
+++ attachment/urls.py
@@ -5,5 +5,6 @@ from django.conf import settings
 urlpatterns = [
        ## authentication paths
     path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
     path('logbook/', update_logbook, name = 'update_logbook'),
 ]
