6749fc3ebf9c2014e006bc5e382f8a0bf8fc2e02
Daniel Karani <danielevans.karani@gmail.com>
2023-08-11 10:07:52 +0300

    Adds the login and update logbook view functions

--- attachment/views.py
+++ attachment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
 from .forms import *
 
 # Create your views here.
@@ -14,7 +15,23 @@ def register(request):
     return render(request, 'attachment/register.html',context)
     
 def login_view(request):
-    pass
+    user = authenticate(request, email="test@gmail.com", password="password@123")
+    if user is not None:
+            login(request, user)
+    
+    return render(request, 'attachment/login.html')
+    
 
 def update_logbook(request):
+    if request.method == 'POST':
+        date = request.POST.get('date')
+        description = request.POST.get('description')
+        LogBook.objects.create(
+            date = date,
+            description = description,
+            student =  request.user.student
+        )
     return render(request, 'attachment/update_logbook.html')
+
+def weekly_summary(request):
+    pass
