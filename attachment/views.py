fe6b55594fada9251791d8b1e3ce82aeb454d1f6
Daniel Karani <danielevans.karani@gmail.com>
2023-08-16 09:20:11 +0300

    Handles succesfull registration

--- attachment/views.py
+++ attachment/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import *
 
 # Create your views here.
 def register(request):
-    
     form = CustomUserCreationForm()
     if request.method == 'POST':
         form  = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('login')
     context = {'form': form}
     
     return render(request, 'attachment/register.html',context)
