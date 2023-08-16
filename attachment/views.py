9bd9cd2f7ad28d4910e61877bcc1a2094921730e
Daniel Karani <danielevans.karani@gmail.com>
2023-08-16 10:09:47 +0300

    Handles registration and login

--- attachment/views.py
+++ attachment/views.py
@@ -6,33 +6,39 @@ from .forms import *
 def register(request):
     form = CustomUserCreationForm()
     if request.method == 'POST':
-        form  = CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     context = {'form': form}
-    
-    return render(request, 'attachment/register.html',context)
-    
+
+    return render(request, 'attachment/register.html', context)
+
+
 def login_view(request):
-    user = authenticate(request, email="test@gmail.com", password="password@123")
-    if user is not None:
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
             login(request, user)
-    
+            if user.user_type == 'student':
+                return redirect('summary')
+
     return render(request, 'attachment/login.html')
-    
+
 
 def update_logbook(request):
     if request.method == 'POST':
         date = request.POST.get('date')
         description = request.POST.get('description')
         LogBook.objects.create(
-            date = date,
-            description = description,
-            student =  request.user.student
+            date=date,
+            description=description,
+            student=request.user.student
         )
     return render(request, 'attachment/update_logbook.html')
 
+
 def weekly_summary(request):
-    return render(request,'attachment/summary.html' )
-    
+    return render(request, 'attachment/summary.html')
